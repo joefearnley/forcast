8a2ea4cfdd3ad87125e3d32f02bf20ced6912721
Joe Fearnley <joe@starboardappdev.com>
2015-07-28 11:03:22 -0400

    Adding a customer weather day directive

--- js/app.js
+++ js/app.js
@@ -4,11 +4,15 @@ app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'pages/home.html',
-            controller: 'homeController'
+            controller: 'HomeController'
         })
         .when('/forecast', {
             templateUrl: 'pages/forecast.html',
-            controller: 'forecastController'
+            controller: 'ForecastController'
+        })
+        .when('/forecast/:days', {
+            templateUrl: 'pages/forecast.html',
+            controller: 'ForecastController'
         });
 });
 
@@ -16,7 +20,7 @@ app.service('cityService', function() {
     this.city = 'Grand Haven, MI';
 });
 
-app.controller('homeController', ['$scope', '$resource', 'cityService',
+app.controller('HomeController', ['$scope', '$resource', 'cityService',
                 function($scope, $resource, cityService) {
     $scope.city = cityService.city;
     $scope.$watch('city', function() {
@@ -24,22 +28,41 @@ app.controller('homeController', ['$scope', '$resource', 'cityService',
     });
 }]);
 
-app.controller('forecastController', ['$scope', '$resource', 'cityService',
-                function($scope, $resource, cityService) {
+app.controller('ForecastController', ['$scope', '$resource', '$routeParams', 'cityService',
+                function($scope, $resource, $routeParams, cityService) {
     $scope.city = cityService.city;
-    var url = 'http://api.openweather.org/data/2.5/forecast/daily';
+    $scope.days = $routeParams.days || 5;
+    var url = 'http://api.openweathermap.org/data/2.5/forecast/daily';
     $scope.weatherApi = $resource(
         url,
         { callback: 'JSON_CALLBACK' },
         { get: { method: 'JSONP' } }
     );
 
-    $scope.result = $scope.weatherApi.get({
+    $scope.results = $scope.weatherApi.get({
         q: $scope.city,
-        cnt: 5
+        cnt: $scope.days
     });
 
-    $scope.convertToFahrenheit = function(degreesInKalvin) {
-        return Math.round((1.8 * (degreesInKalvin - 273)) + 32);
+    $scope.convertToFahrenheit = function(degreesInKelvin) {
+        return Math.round((1.8 * (degreesInKelvin - 273)) + 32);
+    };
+
+    $scope.formatDate = function(dateTime) {
+        return new Date(dateTime * 1000);
     };
 }]);
+
+app.directive('weatherReport', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/weatherReport.html',
+        replace: true,
+        scope: {
+            weatherDay: '=',
+            convertTemperature: '&',
+            convertDate: '&',
+            dateFormat: '@'
+        }
+    }
+});
