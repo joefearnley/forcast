d38de0d5060e8163e40df2a999892075fa362ffb
Joe Fearnley <joe.fearnley@gmail.com>
2015-07-27 23:36:13 -0400

    Adding api data binding

--- js/app.js
+++ js/app.js
@@ -16,13 +16,30 @@ app.service('cityService', function() {
     this.city = 'Grand Haven, MI';
 });
 
-app.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+app.controller('homeController', ['$scope', '$resource', 'cityService',
+                function($scope, $resource, cityService) {
     $scope.city = cityService.city;
-    $scope.watch('city', function() {
+    $scope.$watch('city', function() {
         cityService.city = $scope.city;
     });
 }]);
 
-app.controller('forecastController', ['$scope', 'cityService', function($scope) {
+app.controller('forecastController', ['$scope', '$resource', 'cityService',
+                function($scope, $resource, cityService) {
     $scope.city = cityService.city;
+    var url = 'http://api.openweather.org/data/2.5/forecast/daily';
+    $scope.weatherApi = $resource(
+        url,
+        { callback: 'JSON_CALLBACK' },
+        { get: { method: 'JSONP' } }
+    );
+
+    $scope.result = $scope.weatherApi.get({
+        q: $scope.city,
+        cnt: 5
+    });
+
+    $scope.convertToFahrenheit = function(degreesInKalvin) {
+        return Math.round((1.8 * (degreesInKalvin - 273)) + 32);
+    };
 }]);
